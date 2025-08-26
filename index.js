2240bd020eb8d9ea187faf5e09462e0f6b288fe2
Raven <masoomaqasim5@icloud.com>
2025-08-26 19:19:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Explicit route for root
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('public/home.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
