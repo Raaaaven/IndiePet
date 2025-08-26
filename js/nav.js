6accfcda0eab22bcaff00d4598b4176107866c0c
Raven <masoomaqasim5@icloud.com>
2025-08-26 19:39:34 +0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ let navbar= `
   <nav id="left-navbar">
   <h1 class="logo">IndiePets</h1>
     <ul>
-      <li><a href="home.html"><img src="assets/index.png"></a></li>
+      <li><a href="index.html"><img src="assets/home.png"></a></li>
       <li><a href="pet.html"><img src="assets/petI.png"></a></li>
       <li><a href="badges.html"><img src="assets/badgesI.png"></a></li>
       <li><a href="shop.html"><img src="assets/cart.png"></a></li>
